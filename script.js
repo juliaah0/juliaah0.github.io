6e57e9fbe816d24b60fcc682e5d4afc22d6e7625
juliaah0 <juliaherych@gmail.com>
2024-10-07 17:50:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById("nameButton").addEventListener("click", function() {
     }
   });
   
-  document.getElementById("profilePic").addEventListener("click", function() {
+document.getElementById("profilePic").addEventListener("click", function() {
     this.classList.toggle("enlarged");
   });
   
