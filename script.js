e759007d0ccddfc1f517f67aa5ced30f43637c56
juliaah0 <juliaherych@gmail.com>
2024-10-07 20:01:31 -0400

    update

--- script.js
+++ script.js
@@ -8,4 +8,21 @@ document.getElementById("nameButton").addEventListener("click", function() {
 document.getElementById("profilePic").addEventListener("click", function() {
     this.classList.toggle("enlarged");
   });
-  
+
+
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
